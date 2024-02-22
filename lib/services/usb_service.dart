2228341ee4ee54f38f5cd849ee8d72105cbec396
Azizbek Vahidov <azizbekvahidov@gmail.com>
2024-02-22 09:19:33 +0500

    change

--- lib/services/usb_service.dart
+++ lib/services/usb_service.dart
@@ -2,22 +2,22 @@ import 'dart:io';
 import 'package:flutter_usb_printer/flutter_usb_printer.dart';
 import '../models/usb_printer.dart';
 import '../pos_manager.dart';
-import 'package:printing/printing.dart';
+// import 'package:printing/printing.dart';
 
 class USBService {
   static Future<List<USBPrinter>> findUSBPrinter() async {
     List<USBPrinter> devices = [];
     if (Platform.isWindows || Platform.isMacOS) {
-      var results = await Printing.listPrinters();
+      // var results = await Printing.listPrinters();
       devices = [
-        ...results
-            .where((entry) => entry.isAvailable)
-            .toList()
-            .map((e) => USBPrinter(
-                  name: e.name,
-                  address: e.url,
-                ))
-            .toList()
+        // ...results
+        //     .where((entry) => entry.isAvailable)
+        //     .toList()
+        //     .map((e) => USBPrinter(
+        //           name: e.name,
+        //           address: e.url,
+        //         ))
+        //     .toList()
       ];
     } else if (Platform.isAndroid) {
       var results = await FlutterUsbPrinter.getUSBDeviceList();
