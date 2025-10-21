45e6c1fea27ec3b6e81d08aad4fc700d8bec8ff5
Azizbek Vahidov <azizbekvahidov@gmail.com>
2025-10-21 19:19:28 +0500

    fix

--- lib/enums/connection_type.dart
+++ lib/enums/connection_type.dart
@@ -1,1 +1,8 @@
 enum ConnectionType { network, bluetooth, usb }
+
+ConnectionType ConnectionTypefromString(String value) {
+  return ConnectionType.values.firstWhere(
+    (e) => e.toString().split('.').last.toLowerCase() == value.toLowerCase(),
+    orElse: () => throw Exception('Unknown status: $value'),
+  );
+}
